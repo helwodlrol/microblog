37783fe9049c68af669b279df8954775ca573b57
jc <819379281@qq.com>
2021-05-24 15:50:34 +0800

    2021-02-24: ch05

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,18 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
+# app
 app = Flask(__name__, template_folder='../templates')
+# config
 app.config.from_object(Config)
+# db
 db = SQLAlchemy(app)
+# migrate
 migrate = Migrate(app, db)
+# login
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
